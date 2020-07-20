bb5f2b7773647ad807356302cc1ffe6c4d6aa341
yousufmj <yousufmjaleel@gmail.com>
2020-07-20 14:47:30 +0100

    format visitors

--- src/visitors/index.js
+++ src/visitors/index.js
@@ -9,9 +9,75 @@ export const getToken = async () => {
     console.log(`getting login key`);
     const data = await axios.get(url);
     const key = data.data.token;
+    console.log(key);
 
     return await key;
   } catch (error) {
     throw new Error('Error getting Token');
   }
 };
+
+export const getVisitors = async (page, token) => {
+  try {
+    const url = `${
+      process.env.MOTORWAY_API
+    }/visits?page=${page}&token=${token}`;
+
+    console.log(`getting all Visitors for page ${page}`);
+
+    const data = await axios.get(url);
+
+    return data.data;
+  } catch (error) {
+    throw new Error('Error getting visitors', error);
+  }
+};
+
+export const getAll = async () => {
+  const token = await getToken();
+  let pageNumber = 1;
+  const resultsOne = await getVisitors(pageNumber, token);
+
+  const pages = pageCount(resultsOne.total);
+
+  const visitors = [];
+
+  while (pageNumber <= pages) {
+    visitors.push(await getVisitors(pageNumber, token).then(data => data.data));
+    pageNumber++;
+  }
+
+  return visitors.flat();
+};
+
+export const pageCount = total => {
+  const resultsPerPage = 15;
+  const pages = total / resultsPerPage;
+  return Math.round(pages + 1);
+};
+
+export const formatResults = visitors => {
+  return visitors
+    .filter(visitor => {
+      const day = new Date(visitor.date);
+      return day.getDay() !== 6 && day.getDay() !== 0;
+    })
+    .map(visitor => visitor.name)
+    .reduce((allNames, name) => {
+      if (name in allNames) {
+        allNames[name]++;
+      } else {
+        allNames[name] = 1;
+      }
+      return allNames;
+    }, {});
+};
+
+const start = async () => {
+  const visitors = await getAll();
+
+  const format = formatResults(visitors);
+  console.log(format);
+};
+
+start();
--- src/visitors/visitors.test.js
+++ src/visitors/visitors.test.js
@@ -1,4 +1,4 @@
-import { getToken } from './index';
+import { getToken, getVisitors, formatResults } from './index';
 import axios, { AxiosResponse } from 'axios';
 
 jest.mock('axios');
@@ -15,4 +15,41 @@ test('should fetch token', async () => {
 
   const result = await getToken();
   return expect(result).toEqual(token);
+});
+
+test('should fetch visitors', async () => {
+  const mockData = {
+    total: 100,
+    data: [
+      { id: 1, name: 'Bill Murray', date: '2018-09-02T09:11:00' },
+      { id: 2, name: 'John Doe', date: '2018-08-30T03:24:00' }
+    ]
+  };
+  const mockPageNo = 1;
+  const mockToken = 124;
+
+  axios.get.mockResolvedValue(mockData);
+
+  const result = await getVisitors(mockPageNo, mockToken);
+  console.log(result);
+
+  return expect(result).toEqual(mockData);
+});
+
+test.only('should format results', () => {
+  const visitors = [
+    { id: 1, name: 'Bill Murray', date: '2020-07-20T09:11:00' },
+    { id: 1, name: 'Bill Murray', date: '2018-09-02T09:11:00' },
+    { id: 2, name: 'John Doe', date: '2018-08-30T03:24:00' }
+  ];
+
+  const mockResults = {
+    'Bill Murray': 1,
+    'John Doe': 1
+  };
+
+  const result = formatResults(visitors);
+  console.log(result);
+
+  return expect(result).toEqual(mockResults);
 });
